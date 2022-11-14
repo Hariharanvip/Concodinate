cf11e6aef8bf67396718d91a2381ec209d58c39b
hariharanvip <hariharan1862001@gmail.com>
2022-11-14 12:20:49 +0530

    Concadinate

---
+++ main.js
@@ -0,0 +1,11 @@
+function fu(){
+    var text1 = document.getElementById("text1").value;
+    var text2 = document.getElementById("text2").value;
+    var text3 = document.getElementById("text3").value;
+
+   
+    document.getElementById("text4").value = text1 +" " + text2+ ". "+text3;
+}
+function fgh(){
+    document.getElementById("demo").innerHTML ="<input type='text' id='text4'>"
+}  
